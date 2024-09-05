6fa74340adefe7363374ae9459aa510ecf2e36c3
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-09-05 19:04:15 +0600

    Update slot.js

--- scripts/cmds/slot.js
+++ scripts/cmds/slot.js
@@ -14,17 +14,23 @@ module.exports = {
   langs: {
     en: {
       invalid_amount: "Put a big 🌝 number, you can win twice the risk of my son 🌝🙌",
-      not_enough_money: "You have this amount, see your balance then 🌝🤣",
+      not_enough_money: "You don't have enough money 🌝🤣",
       spin_message: "continued Rotation 🌝",
-      win_message:"You win %1$💗!",
+      win_message: "You win %1$💗!",
       lose_message: "You lost %1$🥲.",
-      jackpot_message: "This is the amount that Diem won! tripartite %1 $!",
+      jackpot_message: "This is the amount that Diem won! tripartite %1$!",
     },
   },
   onStart: async function ({ args, message, event, envCommands, usersData, commandName, getLang }) {
     const { senderID } = event;
     const userData = await usersData.get(senderID);
-    const amount = parseInt(args[0]);
+    
+    let amount;
+    if (args[0] === "all") {
+      amount = userData.money;
+    } else {
+      amount = parseInt(args[0]);
+    }
 
     if (isNaN(amount) || amount <= 0) {
       return message.reply(getLang("invalid_amount"));
@@ -53,16 +59,22 @@ module.exports = {
 };
 
 function calculateWinnings(slot1, slot2, slot3, betAmount) {
-  if (slot1 === "🍒" && slot2 === "🍒" && slot3 === "🍒") {
-    return betAmount * 10;
-  } else if (slot1 === "🍇" && slot2 === "🍇" && slot3 === "🍇") {
-    return betAmount * 5;
-  } else if (slot1 === slot2 && slot2 === slot3) {
-    return betAmount * 3;
-  } else if (slot1 === slot2 || slot1 === slot3 || slot2 === slot3) {
-    return betAmount * 2;
+  const winChance = Math.random();
+
+  if (winChance <= 0.7) {
+    if (slot1 === "🍒" && slot2 === "🍒" && slot3 === "🍒") {
+      return betAmount * 10;
+    } else if (slot1 === "🍇" && slot2 === "🍇" && slot3 === "🍇") {
+      return betAmount * 5;
+    } else if (slot1 === slot2 && slot2 === slot3) {
+      return betAmount * 3;
+    } else if (slot1 === slot2 || slot1 === slot3 || slot2 === slot3) {
+      return betAmount * 2;
+    } else {
+      return betAmount; // Win back the bet amount
+    }
   } else {
-    return -betAmount;
+    return -betAmount; // Lose the bet
   }
 }
 
